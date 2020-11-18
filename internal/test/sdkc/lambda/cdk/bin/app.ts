734918c6d188a63903db55123b20d5a8a091b1ef
Mario Toffia <mario.toffia@gmail.com>
2020-11-18 16:11:55 +0100

    added CDK and deployment

--- internal/test/sdkc/lambda/cdk/bin/app.ts
+++ internal/test/sdkc/lambda/cdk/bin/app.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import * as cdk from '@aws-cdk/core';
-import { DeviceService } from '../lib/index';
+import {TestLambda} from '../lib/index'
 
 const app = new cdk.App();
-var stack = new cdk.Stack(app, 'deviceservicestack');
-new DeviceService(stack, 'deviceservice');
+new TestLambda(app, 'TestLambdaStack');
+app.synth();
--- internal/test/sdkc/lambda/cdk/lib/index.ts
+++ internal/test/sdkc/lambda/cdk/lib/index.ts
@@ -2,16 +2,17 @@ import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import path = require("path");
 
-export class DeviceService extends cdk.Stack {
+const GREENGRASS_EXECUTABLE = new lambda.Runtime('arn:aws:greengrass:::runtime/function/executable')
+export class TestLambda extends cdk.Stack {
 
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
     const testlambda = new lambda.Function(this, 'testlambda', {
-      runtime: lambda.Runtime.GO_1_X,
+      runtime: GREENGRASS_EXECUTABLE,
       functionName: 'testlambda',
       handler: 'testlambda',
-      code: lambda.Code.fromAsset(path.join(__dirname, '/../_out/testlambda')),
+      code: lambda.Code.fromAsset(path.join(__dirname, '../../_out/testlambda')),
       timeout: cdk.Duration.seconds(30),
       environment: {
         IS_ENV_ON_GGC_SET: 'yes they are!',
@@ -20,3 +21,4 @@ export class DeviceService extends cdk.Stack {
 
   }
 }
+
